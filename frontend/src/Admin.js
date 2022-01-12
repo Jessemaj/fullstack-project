897c02054dd142f2636280d69afdec306af82b77
Jesse Majaranta <jesse.majaranta@tuni.fi>
2022-01-12 15:17:57 +0200

    Add word list and ability to add new words

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Admin = () => {
-  return <p>Edit here</p>;
+  const url = "http://localhost:8080/words";
+  const [words, updateWords] = useState([]);
+  const [newEnglishWord, setNewEnglishWord] = useState([]);
+  const [newFinnishWord, setNewFinnishWord] = useState([]);
+
+  const save = () => {
+    var wordInfo = {
+      english_word: newEnglishWord,
+      finnish_word: newFinnishWord,
+    };
+
+    fetch("http://localhost:8080/words/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(wordInfo),
+    }).then(() => refreshPage());
+  };
+
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  const getWords = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => updateWords(data));
+  };
+
+  useEffect(() => {
+    getWords();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [true]);
+
+  return (
+    <>
+      <div className="addWord">
+        <h5>Add a word pair to the excercise here</h5>
+        <input
+          type="text"
+          className="inputField"
+          placeholder="Type english word here"
+          onChange={(obj) => setNewEnglishWord(obj.target.value)}
+        />
+
+        <input
+          type="text"
+          className="inputField"
+          placeholder="Type finnish word here"
+          onChange={(obj) => setNewFinnishWord(obj.target.value)}
+        />
+
+        <button className="button" onClick={save}>
+          Save
+        </button>
+      </div>
+
+      <h5>List of words in the excercise</h5>
+      <table className="adminWordsList">
+        {words.map((wordValues, index) => {
+          return (
+            <tr key={index}>
+              <td className="englishWord">{wordValues.english_word}</td>
+
+              <td className="finnishWord">{wordValues.finnish_word}</td>
+              <button className="button">Edit</button>
+              <button className="button">Delete</button>
+            </tr>
+          );
+        })}
+      </table>
+    </>
+  );
 };
 
 export default Admin;
